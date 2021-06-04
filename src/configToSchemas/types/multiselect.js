0203ca039fb62b14c194e090cef108ec6151629b
Arno Saine <arno@mowhi.com>
2021-06-04 10:43:41 +0300

    Deprecate selectWidget and multiselectWidget

--- src/configToSchemas/types/multiselect.js
+++ src/configToSchemas/types/multiselect.js
@@ -1,13 +1,25 @@
+import deprecate from 'util-deprecate';
 import extendType from './_extendType';
 import select from './select';
 
+const multiselectWidget = deprecate(({ multiselectWidget, ...config }) => {
+  return {
+    widget: { 0: 'checkboxes' }[multiselectWidget],
+    ...config,
+  };
+}, 'config.multiselectWidget is deprecated. Use config.widget instead. Example: "widget": "checkboxes"');
+
 export default extendType(select, ({ config }) => (props) => {
+  if (config.multiselectWidget !== undefined) {
+    config = multiselectWidget(config);
+  }
+
   props.schema = {
     items: props.schema,
     type: 'array',
     uniqueItems: true,
   };
   props.uiSchema = {
-    'ui:widget': { 0: 'checkboxes' }[config.multiselectWidget],
+    'ui:widget': config.widget,
   };
 });
--- src/configToSchemas/types/select.js
+++ src/configToSchemas/types/select.js
@@ -1,15 +1,30 @@
-export default ({
-  config: { choices, choicesDisabled, placeholder, selectWidget },
-}) => ({
-  schema: {
-    enum: choices.map((v, i) => v?.enum || String(i)),
-    enumNames: choices.map((v) => v?.enumNames || v),
-    type: 'string',
-  },
-  uiSchema: {
-    'ui:placeholder': placeholder,
-    'ui:widget': { 0: 'radio' }[selectWidget],
-    //'ui:field': autocomplete ? Autocomplete : undefined,
-    'ui:enumDisabled': choicesDisabled,
-  },
-});
+import deprecate from 'util-deprecate';
+
+const selectWidget = deprecate(({ selectWidget, ...config }) => {
+  return {
+    widget: { 0: 'radio' }[selectWidget],
+    ...config,
+  };
+}, 'config.selectWidget is deprecated. Use config.widget instead. Example: "widget": "radio"');
+
+export default ({ config }) => {
+  if (config.selectWidget) {
+    config = selectWidget(config);
+  }
+
+  const { choices, choicesDisabled, placeholder, widget } = config;
+
+  return {
+    schema: {
+      enum: choices.map((v, i) => v?.enum || String(i)),
+      enumNames: choices.map((v) => v?.enumNames || v),
+      type: 'string',
+    },
+    uiSchema: {
+      'ui:placeholder': placeholder,
+      'ui:widget': widget,
+      //'ui:field': autocomplete ? Autocomplete : undefined,
+      'ui:enumDisabled': choicesDisabled,
+    },
+  };
+};
