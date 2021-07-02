186d4da413fa92c8113737e9822a72aa8505d3fe
Arno Saine <arno@mowhi.com>
2021-07-02 15:59:41 +0300

    Autocomplete updates
     - Connect Autocomplete to value and onChange props
     - Allow values outside the choices

--- src/configToSchemas/typePlugins/autocomplete.js
+++ src/configToSchemas/typePlugins/autocomplete.js
@@ -5,29 +5,40 @@ import { TextField, Chip } from '@material-ui/core';
 export const types = ['select', 'multiselect'];
 
 function AutocompleteField(props) {
-  return <Autocomplete
-    multiple
-    options={props.schema.items.enumNames}
-    defaultValue={[]}
-    freeSolo
-    renderTags={(value, getTagProps) =>
-      value.map((option, index) => (
-        <Chip variant="outlined" label={option} {...getTagProps({ index })} />
-      ))
-    }
-    renderInput={(params) => (
-      <TextField {...params} label={props.schema.title} placeholder={props.schema.title} />
-    )}
-  />;
+  return (
+    <Autocomplete
+      multiple
+      options={props.schema.items.enumNames}
+      value={props.formData ?? []}
+      onChange={(event, value) => props.onChange(value)}
+      freeSolo
+      renderTags={(value, getTagProps) =>
+        value.map((option, index) => (
+          <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+        ))
+      }
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={props.schema.title}
+          placeholder={props.schema.title}
+        />
+      )}
+    />
+  );
 }
 
 export const converter = (config) => (props) => {
   if (config.autocomplete) {
+    // Allow values outside the choices
+    delete props.schema.items.enum;
+
     props.uiSchema ??= {};
     props.uiSchema['ui:field'] = AutocompleteField;
 
     // To let AutocompleteField know if this is select or multiselect
-    props.uiSchema['ui:options'] ??= {};
-    props.uiSchema['ui:options'].type = config.type;
+    // For now only multiselect is supported
+    // props.uiSchema['ui:options'] ??= {};
+    // props.uiSchema['ui:options'].type = config.type;
   }
 };
