a3426b5da282a9f833971b86291f6c103c21b62b
Arno Saine <arno@mowhi.com>
2021-10-05 22:10:50 +0300

    Whitespace

--- src/configToSchemas/commonPlugins/index.js
+++ src/configToSchemas/commonPlugins/index.js
@@ -3,6 +3,7 @@ import { flow } from 'lodash';
 import base from './base';
 import fieldTitleSupport from './fieldTitleSupport';
 import repeatableFieldSupport from './repeatableFieldSupport';
+
 const converters = [base, fieldTitleSupport, repeatableFieldSupport];
 
 export default (config) =>
