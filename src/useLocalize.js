2de6f65fbbac01f419ad643d64fbff6fba61edfb
Ville <ville.ekholm@visma.com>
2022-02-15 13:51:46 +0200

    KEHFORM-617 Korjaus tehty

--- src/useLocalize.js
+++ src/useLocalize.js
@@ -1,5 +1,6 @@
 import { useCallback } from 'react';
 import { useIntl } from 'react-intl';
+import { some } from 'lodash/collection';
 
 export function useLocalize() {
   const { locale, defaultLocale } = useIntl();
@@ -7,7 +8,27 @@ export function useLocalize() {
   const [defaultLang] = defaultLocale.split('-');
 
   return useCallback(
-    (messages = {}) => messages[lang] ?? messages[defaultLang] ?? '',
+    (messages = {}) => {
+      if(messages[lang]) {
+        return messages[lang];
+      }
+      else if(messages[defaultLang]) {
+        return messages[defaultLang];
+      }
+      else if(some(messages)) {
+        let [someLang] = [];
+        Object.filter = (obj, predicate) =>
+          Object.keys(obj)
+            .filter( key => predicate(obj[key]) )
+            .reduce( (res, key) => (res[key] = obj[key], res), {} );
+        const newMessages = Object.filter(messages, value => value !== "");
+        someLang = Object.getOwnPropertyNames(newMessages).toString();
+        return someLang.length > 3 ? messages[someLang.slice(0,2)] : messages[someLang];
+
+      } else {
+        return '';
+      }
+    },
     [defaultLang, lang]
   );
 }
