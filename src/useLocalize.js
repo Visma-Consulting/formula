55bc007061faa91ae3ceb0ba22cdf183b6f76015
EssiSupponen <essi.supponen@visma.com>
2022-02-18 16:14:43 +0200

    KEHFORM-617: small fixes

--- src/useLocalize.js
+++ src/useLocalize.js
@@ -7,28 +7,16 @@ export function useLocalize() {
   const [lang] = locale.split('-');
   const [defaultLang] = defaultLocale.split('-');
 
-  return useCallback(
-    (messages = {}) => {
-      if(messages[lang]) {
-        return messages[lang];
-      }
-      else if(messages[defaultLang]) {
-        return messages[defaultLang];
-      }
-      else if(some(messages)) {
-        let [someLang] = [];
-        Object.filter = (obj, predicate) =>
-          Object.keys(obj)
-            .filter( key => predicate(obj[key]) )
-            .reduce( (res, key) => (res[key] = obj[key], res), {} );
-        const newMessages = Object.filter(messages, value => value !== "");
-        someLang = Object.getOwnPropertyNames(newMessages).toString();
-        return someLang.length > 3 ? messages[someLang.slice(0,2)] : messages[someLang];
+  const getSomeTranslation = (messages) => {
+    if (!some(messages)) { return null }
+    let someLang;
+    const newMessages = Object.filter(messages, value => value !== "");
+    someLang = Object.getOwnPropertyNames(newMessages).toString();
+    return someLang.length > 2 ? messages[someLang.slice(0,2)] : messages[someLang];
+  }
 
-      } else {
-        return '';
-      }
-    },
+  return useCallback(
+    (messages = {}) => messages[lang] ?? messages[defaultLang] ?? getSomeTranslation(messages) ?? '',
     [defaultLang, lang]
   );
 }
