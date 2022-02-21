f5bbd82deb2fc4d0d3746b78921d281a382f3f1d
Ville <ville.ekholm@visma.com>
2022-02-21 11:45:28 +0200

    KEHFORM-591 Korjaus tehty

--- src/Review/types/boolean.js
+++ src/Review/types/boolean.js
@@ -1,1 +1,6 @@
-export default ({ formData, schema }) => schema.enumNames?.[formData ? 0 : 1];
+import { FormattedMessage } from 'react-intl';
+
+export default ({ formData, schema }) => {
+  const defaultEnumNames = [schema.enumNames[0] === '' ? [<FormattedMessage defaultMessage="Kyllä" />] : schema.enumNames[0], schema.enumNames[1] === '' ? [<FormattedMessage defaultMessage="Ei" />] : schema.enumNames[1]];
+  return defaultEnumNames?.[formData ? 0 : 1];
+}
