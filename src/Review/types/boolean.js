503137c841417cc77bedc05a324e4c10779af8f7
EssiSupponen <essi.supponen@visma.com>
2022-02-22 15:02:43 +0200

    Remove unnecessary arrays

--- src/Review/types/boolean.js
+++ src/Review/types/boolean.js
@@ -1,6 +1,6 @@
 import { FormattedMessage } from 'react-intl';
 
 export default ({ formData, schema }) => {
-  const defaultEnumNames = [schema.enumNames[0] === '' ? [<FormattedMessage defaultMessage="Kyllä" />] : schema.enumNames[0], schema.enumNames[1] === '' ? [<FormattedMessage defaultMessage="Ei" />] : schema.enumNames[1]];
+  const defaultEnumNames = [schema.enumNames[0] === '' ? <FormattedMessage defaultMessage="Kyllä" /> : schema.enumNames[0], schema.enumNames[1] === '' ? <FormattedMessage defaultMessage="Ei" /> : schema.enumNames[1]];
   return defaultEnumNames?.[formData ? 0 : 1];
 }
