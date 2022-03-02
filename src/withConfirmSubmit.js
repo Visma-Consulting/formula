9c702c964ef1e1e1a0c1cbee3aef47ba494960b4
EssiSupponen <essi.supponen@visma.com>
2022-03-02 14:28:05 +0200

    KEHFORM-540: Add error handling and show messages if submitting failed

--- src/withConfirmSubmit.js
+++ src/withConfirmSubmit.js
@@ -1,45 +1,82 @@
 import { forwardRef, useRef } from 'react';
 import { FormattedMessage } from 'react-intl';
+import { useMutations } from './api';
 import ConfirmDialog, { isCaptchaRequired } from './ConfirmDialog';
 import { chain } from './utils';
 
 export default function withConfirmSubmit(Form) {
-  return forwardRef(({ confirm = true, onPreSubmit, onPostSubmit, ...other }, ref) => {
-    const confirmDialogRef = useRef();
+  return forwardRef(
+    (
+      {
+        confirm = true,
+        formMetaData,
+        onPreSubmit,
+        onSubmit,
+        onPostSubmit,
+        ...other
+      },
+      ref
+    ) => {
+      const { config } = other;
+      const confirmDialogRef = useRef();
 
-    const showConfirm = confirm || isCaptchaRequired(other.config);
+      const { submit } = useMutations();
 
-    return (
-      <>
-        {showConfirm && (
-          <ConfirmDialog
-            title={<FormattedMessage defaultMessage="Lähetetäänkö lomake?" />}
-            ref={confirmDialogRef}
+      onSubmit ??= submit;
+
+      const showConfirm = confirm || isCaptchaRequired(other.config);
+
+      return (
+        <>
+          {showConfirm && (
+            <ConfirmDialog
+              title={<FormattedMessage defaultMessage="Lähetetäänkö lomake?" />}
+              ref={confirmDialogRef}
+              {...other}
+            ></ConfirmDialog>
+          )}
+          <Form
+            ref={ref}
             {...other}
-          ></ConfirmDialog>
-        )}
-        <Form
-          ref={ref}
-          {...other}
-          onPreSubmit={chain([
-            onPreSubmit,
-            showConfirm &&
+            onSubmit={chain([
+              onPreSubmit,
+              showConfirm &&
               ((...args) => confirmDialogRef.current.confirm(...args)),
-            showConfirm &&
+              showConfirm &&
               ((...args) => {
                 confirmDialogRef.current.loading();
                 return args;
-              })
-          ])}
-          onPostSubmit={chain([
-            showConfirm && ((...args) => {
-              confirmDialogRef.current.close();
-              return args;
-            }),
-            onPostSubmit
-          ])}
-        />
-      </>
-    );
-  });
+              }),
+              async (...args) => {
+                const [{ captchaChallenge, formData }] = args;
+                const data = {
+                  status: 'SUBMITTED',
+                  form: {
+                    id: config._id,
+                    rev: config._rev,
+                  },
+                  ...formMetaData,
+                  values: formData,
+                  captchaChallenge,
+                };
+                try {
+                  const response = await onSubmit(data, ...args);
+
+                  if (showConfirm) {
+                    confirmDialogRef.current.close();
+                  }
+
+                  onPostSubmit?.(response, data, ...args);
+                } catch {
+                  if (showConfirm) {
+                    confirmDialogRef.current.error();
+                  }
+                }
+              },
+            ])}
+          />
+        </>
+      );
+    }
+  );
 }
