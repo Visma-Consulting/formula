7450bac138cedf379931e7e4fee9625f73726ae1
EssiSupponen <essi.supponen@visma.com>
2022-03-21 13:56:34 +0200

    KEHFORM-739: Show Ei valintaa if value undefined

--- src/Review/types/boolean.js
+++ src/Review/types/boolean.js
@@ -2,5 +2,5 @@ import { FormattedMessage } from 'react-intl';
 
 export default ({ formData, schema }) => {
   const defaultEnumNames = [schema.enumNames[0] === '' ? <FormattedMessage defaultMessage="Kyllä" /> : schema.enumNames[0], schema.enumNames[1] === '' ? <FormattedMessage defaultMessage="Ei" /> : schema.enumNames[1]];
-  return defaultEnumNames?.[formData ? 0 : 1];
+  return formData === undefined ? <FormattedMessage defaultMessage="Ei valintaa" /> : defaultEnumNames?.[formData ? 0 : 1];
 }
