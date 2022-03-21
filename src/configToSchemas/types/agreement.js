60626bccf973cc814f5f602875dc8a7c94eeb3d6
Ville <ville.ekholm@visma.com>
2022-03-21 10:35:01 +0200

    KEHFORM-626 Elementin luonti

--- src/configToSchemas/types/agreement.js
+++ src/configToSchemas/types/agreement.js
@@ -1,68 +1,46 @@
-import { Typography } from '@material-ui/core';
-import ToggleButton from '@material-ui/lab/ToggleButton';
-import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import { Typography, Checkbox, FormControlLabel } from '@material-ui/core';
 import { defineMessage, useIntl } from 'react-intl';
-import { ensureValueIsAvailable } from '../../utils';
+import { useState } from 'react';
 
-const Switch = ({ onChange, options, schema, value }) => (
-  <>
-    <Typography variant="subtitle1">{schema.title}</Typography>
-    <ToggleButtonGroup
-      exclusive
-      value={value}
-      onChange={(event, value) => {
-        onChange(value);
-      }}
-    >
-      {options.enumOptions.map(({ label, value }, index) => (
-        <ToggleButton
-          key={index}
-          value={value}
-          disabled={schema.readOnly || options.readonly}
-        >
-          {label}
-        </ToggleButton>
-      ))}
-    </ToggleButtonGroup>
-  </>
-);
+function CheckboxWidget({ id, options, schema }) {
+  const intl = useIntl();
+  const [checked, setChecked] = useState(false);
 
-const NOTHING = 'nothing';
+  const { title, label, useLabel } = schema;
 
-const SwitchWithEmptyOption = ({ onChange, options, value, ...other }) => {
+  const handleChange = (e) => {
+    setChecked(e.target.checked);
+  }
+
   return (
-    <Switch
-      onChange={(value) => onChange(value === NOTHING ? undefined : value)}
-      options={{
-        ...options,
-        enumOptions: [
-          {
-            label: useIntl().formatMessage({ defaultMessage: 'Ei valintaa' }),
-            value: NOTHING,
-          },
-          ...options.enumOptions,
-        ],
-      }}
-      value={value ?? NOTHING}
-      {...other}
-    />
+    <>
+      <Typography variant="subtitle1">{options.description}</Typography>
+      <FormControlLabel
+        control={
+          <Checkbox
+            key={id}
+            checked={checked}
+            onChange={handleChange}
+            label={label}
+          />
+        }
+        label={title}/>
+    </>
   );
-};
-export default ({ config }) => {
-  const { default: defaults, yes, no } = config;
+}
 
-
+export default () => {
 
   return {
     schema: {
-      type: 'agreement',
-      enumNames: [yes, no],
-      default: defaults,
+      type: 'boolean',
     },
     uiSchema: {
+      'ui:widget': CheckboxWidget,
     },
   };
 };
+
 
 export const name = defineMessage({
   defaultMessage: 'Suostumus',
