2d1deaa3c53c9584876df793c42d174f1076a2fb
EssiSupponen <essi.supponen@visma.com>
2022-05-16 11:38:56 +0300

    KEHFORM-311: Add support for formgroups and save data

--- src/configToSchemas/types/compose.js
+++ src/configToSchemas/types/compose.js
@@ -1,61 +1,69 @@
 import { defineMessage, useIntl } from 'react-intl';
 import { utils } from '@visma/rjsf-core';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { useFields } from '../../api';
+import { get } from 'lodash';
 
 function Compose(props) {
   const intl = useIntl();
   const { formData } = useContext(utils.Context);
-  const { values, separator } = props.options;
   const fields = useFields();
+  const path = props.id.split('_').slice(1, -1);
+  const usableFormData = path.length > 0 ? get(formData, path) : formData;
 
-  const composedField = values
-    .map(value => {
-      const [key, id] = value ? value.split('.') : ['error', 'error'];
-      return key === 'error' ? { value: undefined } : {
-        value: formData[key],
-        key: key,
-        id: parseInt(id)
-      }
-    })
-    .filter(data => data.value !== undefined)
-    .map(function getStringValue(data) {
-      const field = fields.find(field => field._id === data.id);
-      if (!field) { return undefined }
-      if (Array.isArray(data.value)) {
-        const listValue = data.value.map(dataValue => {
-          return getStringValue({...data, value: dataValue})
-        });
-        return listValue.length > 0 ? listValue.join(separator) : undefined;
-      } else {
-        switch (field.type) {
-          case 'select': case 'multiselect':
-            const choice = field.choices.find(choice => choice.enum === data.value) ?? field.choices[data.value];
-            return choice.enumNames ?? data.value;
-          case 'boolean':
-            if (data.value) {
-              return field.yes && field.yes !== '' ? field.yes : intl.formatMessage({defaultMessage: 'Kyllä'});
-            } else {
-              return field.no && field.no !== '' ? field.no : intl.formatMessage({defaultMessage: 'Ei'});
-            }
-          case 'text': case 'textarea': case 'email': case 'number': case 'password': case 'range': case 'date':
-            return data.value;
-          default:
-            return undefined;
+  useEffect(() => {
+    const { values, separator } = props.options;
+
+    const composedField = values
+      .map(value => {
+        const [key, id] = value ? value.split('.') : ['error', 'error'];
+        return key === 'error' ? { value: undefined } : {
+          value: usableFormData[key],
+          key: key,
+          id: parseInt(id)
         }
-      }
-    })
-    .filter(data => data !== undefined)
-    .join(separator);
+      })
+      .filter(data => data.value !== undefined)
+      .map(function getStringValue(data) {
+        const field = fields.find(field => field._id === data.id);
+        if (!field) { return undefined }
+        if (Array.isArray(data.value)) {
+          const listValue = data.value.map(dataValue => {
+            return getStringValue({...data, value: dataValue})
+          });
+          return listValue.length > 0 ? listValue.join(separator) : undefined;
+        } else {
+          switch (field.type) {
+            case 'select': case 'multiselect':
+              const choice = field.choices.find(choice => choice.enum === data.value) ?? field.choices[data.value];
+              return choice.enumNames ?? data.value;
+            case 'boolean':
+              if (data.value) {
+                return field.yes && field.yes !== '' ? field.yes : intl.formatMessage({defaultMessage: 'Kyllä'});
+              } else {
+                return field.no && field.no !== '' ? field.no : intl.formatMessage({defaultMessage: 'Ei'});
+              }
+            case 'text': case 'textarea': case 'email': case 'number': case 'password': case 'range': case 'date': case 'dateRange':
+              return data.value;
+            default:
+              return undefined;
+          }
+        }
+      })
+      .filter(data => data !== undefined)
+      .join(separator);
 
-  return (<p>
-    {composedField}
-  </p>);
+    setTimeout(() => {
+      props.onChange(composedField);
+    });
+  }, [usableFormData, props.options, formData, fields])
+
+  return null;
 }
 
 export default (props) => {
   const {
-    config: { values = [], separator = '' },
+    config: { composeValues = [], composeSeparator = '' },
   } = props;
   return {
     schema: {
@@ -64,8 +72,8 @@ export default (props) => {
     uiSchema: {
       'ui:widget': Compose,
       'ui:options': {
-        values,
-        separator
+        values: composeValues,
+        separator: composeSeparator
       },
     },
   };
