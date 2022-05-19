733d6b3156c6f970c65bcaf67afe7cad19bec453
Arno Saine <arno@mowhi.com>
2022-05-19 11:27:07 +0300

    Formatting

--- src/Review/types/object.js
+++ src/Review/types/object.js
@@ -1,4 +1,3 @@
-import Field from '../Field';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,6 +6,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { format } from 'date-fns';
 import { useFormulaContext } from '../../Context';
+import Field from '../Field';
 
 export default (props) => {
   // eslint-disable-next-line prefer-const
@@ -16,51 +16,60 @@ export default (props) => {
 
   // formGroups
   if (uiSchema['ui:options']?.element.type === 'dateRange') {
-    const start = formData.start ? format(new Date(formData.start), 'P', { locale: dateFnsLocale }) : "";
-    const end = formData.end ? format(new Date(formData.end), 'P', { locale: dateFnsLocale }) : "";
+    const start = formData.start
+      ? format(new Date(formData.start), 'P', { locale: dateFnsLocale })
+      : '';
+    const end = formData.end
+      ? format(new Date(formData.end), 'P', { locale: dateFnsLocale })
+      : '';
     const dateValue = `${start} - ${end}`;
     return (
       <Field
         key={ui.id}
         formData={dateValue}
-        schema={{schema, type: 'string' }}
+        schema={{ schema, type: 'string' }}
         uiSchema={uiSchema}
-      />);
+      />
+    );
   } else {
     if (uiSchema['ui:order']) {
-      return (uiSchema['ui:order'].map((name) => (
+      return uiSchema['ui:order'].map((name) => (
         <Field
           key={name}
           formData={formData?.[name]}
           schema={schema.properties[name]}
           uiSchema={uiSchema?.[name]}
-        />)));
+        />
+      ));
     }
   }
 
-
   // tables
   if (schema.properties?.table) {
-    const columns = uiSchema['ui:options'].element.tableColumns
-    return (<TableContainer>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            {columns.map((item, index) => <TableCell key={'header-'+index}>
-              {item}
-            </TableCell>)}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {formData.table?.map((rowItem, rowIndex) => (
-            <TableRow key={'row-'+rowIndex}>{rowItem.map((item, itemIndex) => (
-              <TableCell key={'cell-'+rowIndex+'-'+itemIndex}>
-                {item}
-              </TableCell>
-            ))}</TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>);
+    const columns = uiSchema['ui:options'].element.tableColumns;
+    return (
+      <TableContainer>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              {columns.map((item, index) => (
+                <TableCell key={'header-' + index}>{item}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {formData.table?.map((rowItem, rowIndex) => (
+              <TableRow key={'row-' + rowIndex}>
+                {rowItem.map((item, itemIndex) => (
+                  <TableCell key={'cell-' + rowIndex + '-' + itemIndex}>
+                    {item}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
   }
-}
+};
