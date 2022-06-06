13c166d91f261a57ec7df6f3e32ce72407897092
EssiSupponen <essi.supponen@visma.com>
2022-06-06 16:01:23 +0300

    KEHFORM-811: hetu validation

--- src/configToSchemas/types/text.js
+++ src/configToSchemas/types/text.js
@@ -39,27 +39,46 @@ export const validators = {
       Henkilötunnus on muotoa PPKKVVSNNNT, jossa
       PPKKVV on henkilön syntymäpäivä
       S on vuosisataa esittävä merkki: +, - tai A
-      NNN on yksilönumero väliltä 002-899, parillinen nainen ja pariton mies
+      NNN on yksilönumero väliltä 002-899 (tai 900-999 väliaikaisille hetuille), parillinen nainen ja pariton mies
       T on tarkiste, joka lasketaan jakamalla PPKKVVNNN syntyvä luku luvulla 31
        */
 
       if (value) {
-        if (value.includes('aaa')) {
-          return validationMessages.hetuError;
-        }
+        const tArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'A', 'B', 'C', 'D', 'E', 'F', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y'];
 
         if (value.length !== 11) {
           // Väärän pituinen
           return validationMessages.hetuError;
         }
 
-        if (false) {
+        const decade = value[6];
+
+        if (!['+', '-', 'A'].includes(decade)) {
+          // Väärä vuosisataa esittävä merkki
+          return validationMessages.hetuError;
+        }
+
+        const dateString = `${decade === '+' ? 18 : decade === '-' ? 19 : 20}${value.slice(4,6)}-${value.slice(2,4)}-${value.slice(0,2)}`
+        const date = new Date(dateString);
+
+        if (isNaN(date.getTime()) || dateString !== date.toISOString().slice(0,10)) {
           // Päivämäärä ei ole validi
           return validationMessages.hetuError;
         }
 
-        if (!['+', '-', 'A'].includes(value[6])) {
-          // Väärä vuosisataa esittävä merkki
+        const personalNumber = value.slice(7, 10);
+
+        if (isNaN(personalNumber) || parseInt(personalNumber) > 999 || parseInt(personalNumber) < 2) {
+          // Yksilönumero ei ole validi
+          return validationMessages.hetuError;
+        }
+
+        const tCheckString = value.slice(0, 6) + value.slice(7, 10);
+
+        if (isNaN(tCheckString) || tArray[parseInt(tCheckString) % 31] !== value[10]) {
+          // Tarkiste ei ole oikea
           return validationMessages.hetuError;
         }
       }
