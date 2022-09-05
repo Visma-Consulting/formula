0b25d261f8e7b6cb2da065631018f9c18980715f
Ville <ville.ekholm@visma.com>
2022-09-05 14:46:28 +0300

    KEHFORM-1065 Changed document-object references

--- src/withScrollToError.js
+++ src/withScrollToError.js
@@ -3,10 +3,11 @@ import { forwardRef, useRef } from 'react';
 export default function withScrollToError(Form) {
   return forwardRef((props, ref) => {
     const {onError, ...otherProps} = props;
-    const formWrapperRef = useRef();
+    const formWrapperRef = useRef(null);
 
     const scrollOnError = (...args) => {
-      const errorListElement = document.getElementById('formula-validation-errors');
+      //const errorListElement = document.getElementById('formula-validation-errors');
+      const errorListElement = formWrapperRef.current?.querySelector('#' + 'formula-validation-errors');
       errorListElement?.focus();
       errorListElement?.scrollIntoView({behavior: 'smooth'});
       onError?.(...args);
