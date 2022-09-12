1de06bd237abc9f242470d52e31dd771a8629aa8
Otto Koivisto <otto.koivisto@vismaconsulting.fi>
2022-09-12 17:48:43 +0300

    KEHFORM-623: add element filter normalization in to a list conditions

--- src/useNormalizeConfig/legacyConfigSupport/filter.js
+++ src/useNormalizeConfig/legacyConfigSupport/filter.js
@@ -35,18 +35,49 @@ const handleOther = ({ target, exists, equals, unequals, ...other }) => ({
 });
 
 export default function filter(config) {
+
+  const isSingleRule = (query) => Object.keys(query).includes("$or") || Object.keys(query).includes("$and");
+
+  const handleSingleRule = (query) =>
+    //['$or', '$and'].includes(Object.keys(query))
+    isSingleRule(query)
+      ? query
+      : { $or: [query] };
+
   if (typeof config.filter?.show?.target === 'number') {
     config.filter.show.target = String(config.filter.show.target);
   }
   if (typeof config.filter?.enable?.target === 'number') {
     config.filter.enable.target = String(config.filter.enable.target);
   }
-  if (config.filter) {
+  if (config.filter && Object.keys(config.filter).length > 0) {
     for (const type of ['show', 'enable']) {
       if (config.filter[type]?.target) {
         config.filter[type] = {
-          query: handleLegacyFilter(config.filter[type]),
+          query: handleSingleRule(handleLegacyFilter(config.filter[type])),
         };
+      }
+
+      // console.log("---");
+      // console.log(config.filter[type]);
+      // console.log(config.filter);
+      // console.log("---");
+
+      if (config.filter[type]) {
+
+        // console.log("BEFORE");
+        // console.log(config.filter[type]);
+        // console.log(isSingleRule(config.filter[type].query));
+        // console.log(['$or', '$and'].includes(Object.keys(config.filter[type].query)));
+
+        config.filter[type] = {
+          query: handleSingleRule(config.filter[type].query)
+        };
+
+        // console.log("-");
+        // console.log(config.filter[type]);
+        // console.log(config.filter);
+        // console.log("-");
       }
     }
   }
