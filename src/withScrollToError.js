c0ca0774a5318917f6f65de3e34950b7ea40faa3
EssiSupponen <essi.supponen@visma.com>
2022-09-26 11:48:40 +0300

    KEHFORM-1129: hotfix

--- src/withScrollToError.js
+++ src/withScrollToError.js
@@ -3,16 +3,16 @@ import { forwardRef, useRef } from 'react';
 export default function withScrollToError(Form) {
   return forwardRef((props, ref) => {
     const {onError, ...otherProps} = props;
-    const formWrapperRef = useRef(ref);
+    const formWrapperRef = useRef();
 
     const scrollOnError = (...args) => {
       //const errorListElement = document.getElementById('formula-validation-errors');
-      const errorListElement = formWrapperRef.current?.formElement?.querySelector('#' + 'formula-validation-errors');
+      const errorListElement = formWrapperRef.current?.querySelector('#' + 'formula-validation-errors');
       errorListElement?.focus();
       errorListElement?.scrollIntoView({behavior: 'smooth'});
       onError?.(...args);
     }
 
-    return <Form ref={formWrapperRef} onError={scrollOnError} {...otherProps} />
+    return <div ref={formWrapperRef}><Form ref={ref} onError={scrollOnError} {...otherProps} /></div>
   })
 }
