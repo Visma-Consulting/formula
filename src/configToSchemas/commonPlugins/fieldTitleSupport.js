36afc8a75a2d5f7f28f560d30759db697a5c2899
Ville <ville.ekholm@visma.com>
2023-01-18 08:46:32 +0200

    KEHFORM-1221 Logon lisäys mahdollisuus lomake editorissa

--- src/configToSchemas/commonPlugins/fieldTitleSupport.js
+++ src/configToSchemas/commonPlugins/fieldTitleSupport.js
@@ -1,3 +1,23 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: '15%',
+    maxHeight: '30px',
+    float: 'right',
+  },
+});
+
+function Image(logoUrl) {
+  const classes = useStyles();
+
+  return <img src={logoUrl} alt={'Logo'} className={classes.root} />;
+}
+
 export default (config) => (props) => {
-  props.schema.title ??= config.useLabel ? config.label : config.title;
+  props.schema.title ??= config.useLabel ? config.label : config.logo ?
+    <div>
+    {config.title}
+    {Image(config.logo)}
+  </div> : config.title;
 };
