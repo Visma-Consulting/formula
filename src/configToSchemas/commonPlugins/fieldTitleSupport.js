8e4e179a55b6e348f6c18e74401b8d24b8be3fb9
Ville <ville.ekholm@visma.com>
2023-01-18 14:33:08 +0200

    KEHFORM-1221 Logon css päivitys ja maksimi tiedostokoko

--- src/configToSchemas/commonPlugins/fieldTitleSupport.js
+++ src/configToSchemas/commonPlugins/fieldTitleSupport.js
@@ -1,22 +1,25 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-  root: {
+  image: {
     maxWidth: '15%',
-    maxHeight: '30px',
+    maxHeight: '100px',
+    width: 'inherit',
+    height: 'inherit',
     float: 'right',
+    objectFit: 'contain'
   },
 });
 
 function Image(logoUrl) {
   const classes = useStyles();
 
-  return <img src={logoUrl} alt={'Logo'} className={classes.root} />;
+  return <img src={logoUrl} alt={'Logo'} className={classes.image} />;
 }
 
 export default (config) => (props) => {
   props.schema.title ??= config.useLabel ? config.label : config.logo ?
-    <div>
+    <div style={{ overflow: 'hidden' }}>
     {config.title}
     {Image(config.logo)}
   </div> : config.title;
