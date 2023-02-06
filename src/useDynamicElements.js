4b37b6c50485a048fa0657a90f0aa7da9d7337be
EssiSupponen <essi.supponen@visma.com>
2023-02-06 10:06:16 +0200

    KEHFORM-1242: Fix bug when mapping list elements

--- src/useDynamicElements.js
+++ src/useDynamicElements.js
@@ -80,10 +80,9 @@ function resetDisabledToDefaultValues(formData, initialFormData, config, allDisa
       if (element.skipDisable) {
         resetFormData[key] = formData[key];
       } else if (element.list) {
-        resetFormData[key] = formData[key];
         resetFormData[key] = formData[key].map((datum, i) => resetDisabledToDefaultValues(
           datum ?? undefined,
-          initialFormData ? initialFormData[key][i] : undefined,
+          initialFormData?.[key] ? initialFormData[key][i] : undefined,
           element,
           false
         ))
