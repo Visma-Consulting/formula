8d52b7da31828c8f954b240798c61475adfca1ee
Ville <ville.ekholm@visma.com>
2023-03-16 09:55:11 +0200

    Nappi kysymykselle mahdollisuus palauttaa onnistuneen painalluksen jälkeen teksti elementti

--- src/configToSchemas/types/button.js
+++ src/configToSchemas/types/button.js
@@ -3,6 +3,8 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import extendType from './_extendType';
 import _dummy from './_dummy';
+import { useState } from 'react';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -13,21 +15,30 @@ const useStyles = makeStyles((theme) => ({
 function ButtonWidget(props) {
   const classes = useStyles();
   const {options} = props;
+  const [showSuccessText, setShowSuccessText] = useState(false)
+  const assignFormSuccessText = options.element?.meta?.buttonActionProps?.assignFormSuccessText;
   const buttonText = options?.element?.useLabel ? options?.element?.label : options?.element?.title
-
   const onClickAction = props.schema?.buttonActions && props.schema?.buttonActions[options.element?.meta?.buttonOnClickAction];
+  const onClick = () => {
+    const response = onClickAction(options.element?.meta?.buttonActionProps)
+    if (response) {
+      setShowSuccessText(true)
+    }
+  }
+
   return (
     <div>
-      <Button
-        variant="contained"
-        color="Primary"
-        size={"small"}
-        disabled={props?.schema?.disabled}
-        className={classes.button}
-        onClick={() => onClickAction && onClickAction(options.element?.meta?.buttonActionProps)}
-      >
-        {buttonText}
-      </Button>
+      {(showSuccessText && assignFormSuccessText) ? <Typography>{assignFormSuccessText}</Typography> :
+        <Button
+          variant="contained"
+          color="Primary"
+          size={"small"}
+          disabled={props?.schema?.disabled}
+          className={classes.button}
+          onClick={onClick}
+        >
+          {buttonText}
+        </Button> }
     </div>
   );
 }
