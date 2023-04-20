48e9ab01128bdbacff6733995889b9649cdce32f
Ville <ville.ekholm@visma.com>
2023-04-20 14:43:02 +0300

    Replace old date component with new one

---
+++ src/configToSchemas/types/date/phrases.js
@@ -0,0 +1,16 @@
+import { defineMessage } from 'react-intl';
+
+const calendarLabel = defineMessage({
+  defaultMessage: 'Kalenteri',
+})
+const jumpToPrevMonth = defineMessage({
+  defaultMessage: 'Edellinen kuukausi',
+})
+const jumpToNextMonth = defineMessage({
+  defaultMessage: 'Seuraava kuukausi',
+})
+export const DateRangePickerPhrases = {
+  calendarLabel,
+  jumpToPrevMonth,
+  jumpToNextMonth,
+}
