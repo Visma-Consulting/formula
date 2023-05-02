5d8741c57c427abd8bc8e2e611dbe6de64f78eb2
EssiSupponen <essi.supponen@visma.com>
2023-05-02 14:18:37 +0300

    KEHFORM-1296: Add missing fill actions

--- src/withRecaptcha.js
+++ src/withRecaptcha.js
@@ -6,14 +6,23 @@ import ReCAPTCHA from 'react-google-recaptcha';
 import { useTheme } from '@material-ui/core';
 import { useConfig } from './client';
 
+const useStyles = makeStyles((theme) => ({
+  recaptcha: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+  buttonContainer: {
+    '& > *': {
+      marginRight: theme.spacing(1),
+      '@media print': {
+        display: 'none',
+      },
+    },
+  }
+}));
+
 export default function withRecaptcha(Form) {
-  const useStyles = makeStyles((theme) => ({
-    recaptcha: {
-      marginTop: theme.spacing(1),
-      marginBottom: theme.spacing(1),
-    }
-  }));
-  return forwardRef(({...otherProps}, ref) => {
+  return forwardRef(({fillProps, ...otherProps}, ref) => {
     const intl = useIntl();
     const classes = useStyles();
     const theme = useTheme().palette.type;
@@ -21,8 +30,10 @@ export default function withRecaptcha(Form) {
     const { recaptcha } = useConfig();
     if (otherProps?.config?.publicForm && (otherProps?.isLastStep === true || otherProps?.isLastStep === undefined)) {
       return (
-        <Form ref={ref} {...otherProps} captcha={captchaChallenge} >
+        <Form ref={ref} fillProps={fillProps} {...otherProps} captcha={captchaChallenge} >
           <ReCAPTCHA sitekey={recaptcha.sitekey} theme={theme} hl={intl.locale?.split('-')[0]} onChange={setCaptchaChallenge} className={classes.recaptcha} />
+          <div className={classes.buttonContainer}>
+            {fillProps?.actions ?? null}
             <Button
               type="submit"
               variant="contained"
@@ -31,6 +42,7 @@ export default function withRecaptcha(Form) {
             >
               <FormattedMessage defaultMessage="Lähetä" />
             </Button>
+          </div>
         </Form>
       )
     } else {
