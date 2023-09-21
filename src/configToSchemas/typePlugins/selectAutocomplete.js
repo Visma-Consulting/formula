306c713a0060abc47136ab1d411fe0113f9788f9
EssiSupponen <essi.supponen@visma.com>
2023-09-21 16:01:44 +0300

    KEHFORM-1213: Fix labels and aria-labels

--- src/configToSchemas/typePlugins/selectAutocomplete.js
+++ src/configToSchemas/typePlugins/selectAutocomplete.js
@@ -3,20 +3,46 @@ import { Autocomplete } from '@material-ui/lab';
 import { utils } from '@visma/rjsf-core';
 import produce from 'immer';
 import { useContext } from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  inputLabelRoot: {
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    whiteSpace: "nowrap",
+    width: 1
+  },
+  inputFormControl: {
+    "label + &": {
+      marginTop: 0
+    }
+  }
+});
 
 export const types = ['select', 'selectV2'];
 
 export function AutocompleteSelectField(props) {
+  const classes = useStyles();
   return (
     <Autocomplete
       options={props.schema.enumNames}
-      aria-label={utils.generateAriaLabel(props.schema.title, props.uiSchema['ui:options'], props.schema.required)}
       value={props.schema.enumNames[props.schema.enum.indexOf(props.formData)]}
       onChange={(event, value) =>
         props.onChange(props.schema.enum[props.schema.enumNames.indexOf(value)])
       }
       getOptionSelected={(option, value) => option === value}
-      renderInput={(params) => <TextField {...params} />}
+      renderInput={(params) =>
+        <TextField
+          {...params}
+          hiddenLabel
+          label={utils.generateAriaLabel(props.schema.title, props.uiSchema?.['ui:options'], props.required)}
+          aria-describedby={utils.ariaDescribedBy(props.idSchema.$id, props.uiSchema, props.rawErrors)}
+          InputLabelProps={{shrink: false, className: classes.inputLabelRoot}}
+        />
+      }
     />
   );
 }
