1d5c043fa06135dd7cdcff5746a4d243996740e3
EssiSupponen <essi.supponen@visma.com>
2023-10-09 15:07:41 +0300

    KEHFORM-530: numberOfDecimals

--- src/configToSchemas/types/number.js
+++ src/configToSchemas/types/number.js
@@ -1,7 +1,7 @@
 import { defineMessage } from 'react-intl';
 
 export default ({
-  config: { numberDefault: defaultValue, maximum, minimum, multipleOf },
+  config: { numberDefault: defaultValue, maximum, minimum, multipleOf, numberOfDecimals },
 }) => ({
   schema: {
     default: defaultValue < minimum || defaultValue > maximum ? undefined : defaultValue ?? undefined,
@@ -9,7 +9,8 @@ export default ({
     minimum,
     multipleOf,
     type: ['number', 'null'],
-    allowedCharacters: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '.']
+    allowedCharacters: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '.'],
+    numberOfDecimals: numberOfDecimals
   },
 });
 
