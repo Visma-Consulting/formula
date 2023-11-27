4e5bf1e76a3b5810cb7823ae610fd9354fa519ad
Otto Koivisto <otto.koivisto@gmail.com>
2023-11-27 14:13:25 +0200

    add source maps to debug

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ import { filename, globalVar, version } from './dll.js';
 export default {
   mode: 'production',
   entry: './src/main.js',
+  devtool: "source-map",
   output: {
     path: path.resolve(process.cwd(), 'dist/dll', version),
     library: globalVar,
