7086f9bb26a3973b0dd262bba02789a1f35383d0
EssiSupponen <essi.supponen@twoday.com>
2024-11-27 09:33:40 +0200

    KEHFORM-1516: Fix breaking in editor review

--- src/configToSchemas/types/compose.js
+++ src/configToSchemas/types/compose.js
@@ -37,7 +37,7 @@ const getUsableFormData = (id, element, rootSchema, formData) => {
     }
   }
 
-  return {usableFormData: formData, existingDataKeys: Object.keys(rootSchema.properties)};
+  return {usableFormData: formData, existingDataKeys: Object.keys(rootSchema?.properties ?? {})};
 }
 
 function Compose(props) {
